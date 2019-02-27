f1297664a9c9dcd31da78cf041c78c699bb21dd6
Carissa Wood <carissawood@outlook.com>
2019-02-27 11:15:32 -0800

    Add local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,3 +132,9 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Location to access an image
 MEDIA_URL = '/media/'
+
+# import all the information from local_settings; else, pass by that exception
+try: 
+    from local_settings import * 
+except ImportError:
+    pass
