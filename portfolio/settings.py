d81669fcf056557379e345ed3c21005080de4006
Carissa Wood <carissawood@outlook.com>
2019-02-26 21:23:56 -0800

    Fix blog config in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobs.apps.JobsConfig',
-    'jobs.apps.BlogConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
