169e876be5f6137daacaf329353ac1097ea257d0
Carissa Wood <carissawood@outlook.com>
2019-02-26 21:24:54 -0800

    Add 'max_length' attribute to CharField in model

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     pub_date = models.DateTimeField
-    body = models.CharField()
+    body = models.CharField(max_length=2000)
     image = models.ImageField(upload_to='images/')
