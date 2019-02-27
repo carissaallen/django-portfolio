ee9fa635e3e2b8dde12b3ee144cf31696e948e6e
Carissa Wood <carissawood@outlook.com>
2019-02-27 09:08:51 -0800

    Get framework for blog working

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,18 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    pub_date = models.DateTimeField
+    pubdate = models.DateField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    # display blog titles in admin page
+    def __str__(self):
+        return self.title
+    
+    # shortens body of blog post on blog home page
+    def summary(self):
+        return self.body[:200]+'...'
+
+    # see strftime.org for date customization 
+    def date(self):
+        return self.pubdate.strftime('%b %e %Y')
