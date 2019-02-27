1a90cca5a70aa7a4b1683336934a61c4b468acb9
Carissa Wood <carissawood@outlook.com>
2019-02-26 21:29:12 -0800

    Update blog fields to improve usability

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Blog(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
     pub_date = models.DateTimeField
-    body = models.CharField(max_length=2000)
+    body = models.TextField()
     image = models.ImageField(upload_to='images/')
