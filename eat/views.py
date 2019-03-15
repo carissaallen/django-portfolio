7fd9bdf1746633a285898e0c4b42c6c11b4f9e11
Carissa Allen <carissawood@outlook.com>
2019-03-15 20:42:12 +0000

    Produce random meal result upon button click

--- eat/views.py
+++ eat/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Eat
+from random import choice
 
 def dinner(request):
     food = Eat.objects
     return render(request, 'eat/dinner.html', {'eat':food})
+
+def meal(request):
+    dinner = ['Chicken Tacos', 'Spaghetti', 'Grilled Cheese + Tomato Soup', 'Fettucini Alfredo', 'Roasted Chicken + Butternut Squash + Purple Potatoes', 'Grilled Salmon', 'Steak + Vegetables', 'Grilled Cod + French Fries', 'Chicken Enchiladas', 'Chicken Pot Pie', 'Clam Chowder']
+    meal = choice(dinner)
+    return render(request, 'eat/meal.html', {'food':meal})
